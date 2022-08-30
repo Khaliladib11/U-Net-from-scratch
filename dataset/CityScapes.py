1b9d8c8099c6275a5a5c15ef691ca18215661e41
Khalil Adib <Khaliladib11@gmail.com>
2022-08-30 20:24:09 +0100

    Fix some problems in the Dataset class

--- dataset/CityScapes.py
+++ dataset/CityScapes.py
@@ -7,15 +7,16 @@ from torch.utils import data
 import torchvision.transforms as transforms
 
 from CityScapes_labels import label2id, id2label
+import random
+import matplotlib.pyplot as plt
 
-class CityScapes(data.Dataset)
+
+class CityScapes(data.Dataset):
 
     def __init__(self, img_root, masks_root, list_of_classes, stage):
         super(CityScapes, self).__init__()
 
         assert stage in ['train', 'test', 'val'], "Please the 'stage' parameter must be from ['train', 'test', 'val']"
-
-
 
         self.img_root = os.path.join(img_root, stage)
         self.masks_root = os.path.join(masks_root, stage)
@@ -28,12 +29,13 @@ class CityScapes(data.Dataset)
 
         self.cities = os.listdir(self.img_root)
 
+        self.__create_classes_helper()
+
         self.mean_transform = [0.407, 0.457, 0.485]
-        self.std = [0.229,0.224,0.225]
+        self.std_transform = [0.229, 0.224, 0.225]
 
         self.images = self.__load_images()
         self.masks = self.__load_semantic_mask()
-
 
     def __create_classes_helper(self):
         self.class_to_idx = {}
@@ -53,11 +55,10 @@ class CityScapes(data.Dataset)
 
         return images
 
-
     def __load_semantic_mask(self):
         semantic_masks = deque()
         for city in self.cities:
-            masks = os.listdir(self.masks_root, city)
+            masks = os.listdir(os.path.join(self.masks_root, city))
             for file in masks:
                 if file.endswith('labelIds.png'):
                     semantic_masks.append(file)
@@ -66,29 +67,29 @@ class CityScapes(data.Dataset)
 
     def apply_transform(self, image):
         t_ = transforms.Compose([
-            transforms.Resize(640, 640),
+            # transforms.Resize([640, 640]),
             transforms.ToTensor(),
-            transforms.Normalize(mean = self.mean_transform,
-                                 std = self.std_transform)
+            transforms.Normalize(mean=self.mean_transform,
+                                 std=self.std_transform)
         ])
         img = t_(image)
         return img
 
-    def load_image(self, idx, trasform=True):
+    def get_image(self, idx, transform=True):
         image = self.images[idx]
         city_name = image.split('_')[0]
         image = Image.open(os.path.join(self.img_root, city_name, image))
-        if trasform:
+        if transform:
             image = self.apply_transform(image)
 
         return image
 
     # Get Semantic Mask
-    def load_semantic_mask(self, idx):
+    def get_semantic_mask(self, idx):
         img_file_name = self.images[idx]
         img_file_name = img_file_name.split("_")
-        semantic_mask_file = img_file_name[0]+'_'+img_file_name[1]+'_'+img_file_name[2]+'_gtFine_labelIds.png'
-        #semantic_mask_file = self.semantic_masks[idx]
+        semantic_mask_file = img_file_name[0] + '_' + img_file_name[1] + '_' + img_file_name[2] + '_gtFine_labelIds.png'
+        # semantic_mask_file = self.semantic_masks[idx]
         city_name = semantic_mask_file.split('_')[0]
         mask = np.array(Image.open(os.path.join(self.masks_root, city_name, semantic_mask_file)))
         unique_values = np.unique(mask)
@@ -106,12 +107,33 @@ class CityScapes(data.Dataset)
     def __len__(self):
         return len(self.images)
 
-
     def __getitem__(self, idx):
-        image = self.load_image(idx)
-        mask = self.load_semantic_mask(idx)
+        image = self.get_image(idx)
+        mask = self.get_semantic_mask(idx)
 
         return idx, image, mask
 
 
+if __name__ == "__main__":
+    params = {
+        "img_root": '../data/cityscapes/leftImg8bit',
+        "masks_root": '../data/cityscapes/gtFine',
+        "list_of_classes": ['__bgr__', 'car', 'person'],
+        "stage": 'train'
+    }
 
+    cityscapes = CityScapes(**params)
+
+    print(f"Length: {len(cityscapes)}")
+    idx = random.randint(0, len(cityscapes))
+    idx, image, mask = cityscapes[idx]
+
+    """
+    image = cityscapes.get_image(idx, transform=False)
+    mask = cityscapes.get_semantic_mask(idx)
+    """
+
+    fig, ax = plt.subplots(1, 2, figsize=(10, 10))
+    ax[0].imshow(image.permute(1, 2, 0))
+    ax[1].imshow(mask)
+    plt.show()
